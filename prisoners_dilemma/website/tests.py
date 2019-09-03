2a697d138b6873aaef203bdbe9288e1d92325ab2
sarah-russell12 <sarah.russell12@ncf.edu>
2019-09-03 12:50:31 -0400

    Completed leaderboard tests

--- prisoners_dilemma/website/tests.py
+++ prisoners_dilemma/website/tests.py
@@ -13,22 +13,6 @@ def createPlayer(username, password):
     user.save()
     return
 
-def createExperiencedPlayer(username, password, points=10, games=1, coop_actions=8):
-    """
-    By default creates a user that has played 1 game and earned 10 points by
-    not cooperating when their opponent cooperated twice, and cooperated the
-    other 8 times when their opponent did not.
-    """
-    user = PlayerUser.objects.create_user(username=username)
-    user.set_password(password)
-    user.points = points
-    user.games_completed = games
-    user.cooperative_actions = coop_actions
-    user.save()
-    user.updateCooperativeScore()
-    user.save()
-    return
-
 # Create your tests here.
 class HomeViewTests(TestCase):
     def _get_client_response(self):
@@ -213,6 +197,22 @@ class ChangePasswordViewTests(TestCase):
         self.assertContains(response, "New password")
         self.assertContains(response, "New password confirmation:")
         return
+
+def createExperiencedPlayer(username, password, points=10, games=1, coop_actions=8):
+    """
+    By default creates a user that has played 1 game and earned 10 points by
+    not cooperating when their opponent cooperated twice, and cooperated the
+    other 8 times when their opponent did not.
+    """
+    user = PlayerUser.objects.create_user(username=username)
+    user.set_password(password)
+    user.points = points
+    user.games_completed = games
+    user.cooperative_actions = coop_actions
+    user.save()
+    user.updateCooperativeScore()
+    user.save()
+    return
     
 class LeaderboardViewTests(TestCase):
     def _get_client_response(self):
@@ -249,6 +249,12 @@ class LeaderboardViewTests(TestCase):
         A leaderboard page with no users will give a "There are no players."
         message in the response.
         """
+        response = self._get_client_response()
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Login")
+        self.assertContains(response, "Sign Up")
+        self.assertContains(response, "There are no top players")
         return
         
     def test_leaderboard_with_one_user_no_points(self):
@@ -256,6 +262,14 @@ class LeaderboardViewTests(TestCase):
         A leaderboard page with one user that has no points will give a
         "There is no leaderboard" message in the response
         """
+        createPlayer("usr", "PssWrd123")
+        self.client.login(username="usr", password="PssWrd123")
+        response = self._get_client_response()
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "usr")
+        self.assertContains(response, "Logout")
+        self.assertContains(response, "There are no top players")
         return
     
     def test_leaderboard_with_one_user_with_points(self):
@@ -263,6 +277,24 @@ class LeaderboardViewTests(TestCase):
         A leaderboard page with one user that has points will show a table
         with the user listed
         """
+        createExperiencedPlayer("usr", "PssWrd123")
+        response = self._get_client_response()
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Login")
+        self.assertContains(response, "Sign Up")
+        
+        # table header
+        self.assertContains(response, "Rank")
+        self.assertContains(response, "Username")
+        self.assertContains(response, "Score")
+        self.assertContains(response, "Cooperative Score")
+        
+        # experienced player
+        self.assertContains(response, "1")
+        self.assertContains(response, "usr")
+        self.assertContains(response, "10")
+        self.assertContains(response, "0.8")
         return
     
     def test_leaderboard_with_multiple_users(self):
@@ -271,19 +303,36 @@ class LeaderboardViewTests(TestCase):
         with the users listed by decending order of points.
         The leaderboard page will not display users with no points.
         """
-        return
-    
-    def test_leaderboard_with_multiple_users_same_score(self):
-        """
-        A leaderboard page with multiple users with points will show a table
-        with the users listed by decending order of points.
-        Users that have the same number of points will be given the same rank
-        number, and the next player after them given the usual rank number as
-        their order in the rankings.  A tie for rank 2 should look like this:
-            Rank    User        Points
-            1       usr_1       46
-            2       user_2      30
-            2       user_3      30
-            4       user_4      1
-        """
+        # usr1 played 4 games where they were never cooperative
+        createExperiencedPlayer("usr1", "PssWrd123", points=188, games=4, coop_actions=0)
+        # usr2 played 2 games with usr1.  They were cooperative except for the last round of every game
+        createExperiencedPlayer("usr2", "PssWrd456", points=4, games=2, coop_actions=18)
+        # usr3 has not played any games
+        createPlayer("usr3", "PssWrd789")
+        response = self._get_client_response()
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Login")
+        self.assertContains(response, "Sign Up")
+        
+        # table header
+        self.assertContains(response, "Rank")
+        self.assertContains(response, "Username")
+        self.assertContains(response, "Score")
+        self.assertContains(response, "Cooperative Score")
+        
+        # usr1
+        self.assertContains(response, "1")
+        self.assertContains(response, "usr1")
+        self.assertContains(response, "188")
+        self.assertContains(response, "0")
+        
+        # usr2
+        self.assertContains(response, "2")
+        self.assertContains(response, "usr2")
+        self.assertContains(response, "4")
+        self.assertContains(response, "0.9")
+        
+        # usr3
+        self.assertNotContains(response, "usr3")
         return
